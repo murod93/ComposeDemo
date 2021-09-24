636908413934691b85f0b9b112130b7075eccbb5
minmax9493 <min_max1993@mail.ru>
2021-09-24 23:32:59 +0900

    .

--- app/src/main/java/com/example/android/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/android/composedemo/MainActivity.kt
@@ -4,7 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.android.composedemo.examples.LayoutScaffold
 import com.example.android.composedemo.ui.theme.ComposeDemoTheme
 
@@ -15,9 +23,30 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LayoutScaffold()
+
                 }
             }
         }
+    }
+}
+
+@Composable
+fun GalleryApp() {
+    ComposeDemoTheme {
+
+        // create navController
+        val navController = rememberNavController()
+
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+
+        }
+    }
+}
+
+@Composable
+fun GalleryAppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(navController = navController, startDestination = "/",modifier = modifier) {
+//        composable()
     }
 }
